098847ef4527cae9e149d672b4ae86412c0e34bd
aishlove <aishlove369@hotmail.com>
2021-02-27 21:08:12 +0900

    공부한다

--- Program.cs
+++ Program.cs
@@ -59,29 +59,115 @@ namespace CSharpStudy_0226
     // }
     // #endregion
 
-#region  연습문제 2
-namespace Ex6_2
-{
-    class MainApp
-    {
-        public static void Main()
-        {
-            double mean = 0;
-            Mean(1,2,3,4,5);
-            System.Console.WriteLine("평균 : {0}", mean);
-            
+// #region  연습문제 2
+// namespace Ex6_2
+// {
+//     class MainApp
+//     {
+//         public static void Main()
+//         {
+//             double mean = 0;
+//             Mean(1,2,3,4,5);
+//             System.Console.WriteLine("평균 : {0}", mean);
+
+//             double Mean(double a, double b, double c, double d, double e)
         
-            double Mean(double a, double b, double c, double d, double e)
-            {
+//             {
 
-                mean = (a + b+ c + d+ e )/5;
-                return mean;
-            }    
+//                 mean = (a + b+ c + d+ e )/5;
+//                 return mean;
+//             }    
 
-        }
+//         }
+ 
+//     }
+// }
+// #endregion
 
+// #region  클래스
+// namespace Constructor
+// {
+//     class Cat
+//     {
+//         public Cat()
+//         {
+//             Name = "";
+//             Color = "";
+//         }
+
+//         public Cat(string _Name, string _Color)
+//         {
+//             Name = _Name;
+//             Color = _Color;
+//         }
+
+//         ~Cat()  // 종료자
+//         {
+//             System.Console.WriteLine($"{Name} : 잘가");
+//         }
+
+//         public string Name;
+//         public string Color;
+
+//         public void Meow()
+//         {
+//             System.Console.WriteLine($"{Name} : 야옹");
+//         }
+//     }
+
+//     class MainApp
+//     {
+//         static void Main(string[] args)
+//         {
+//             Cat Kitty = new Cat("키티", "하얀색");
+//             Kitty.Meow();
+//             System.Console.WriteLine($"{Kitty.Name} : {Kitty.Color}");
+
+//             Cat nero = new Cat("네로", "검은색");
+//             nero.Meow();
+//             System.Console.WriteLine($"{nero.Name} : {nero.Color}");
+//         }
+//     }
+// } 
+// #endregion
+
+#region 정적 static
+
+class Global
+{
+    public static int Count = 0;
+}
+
+class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
     }
 }
+
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        System.Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+        
+        System.Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}  
 #endregion
 
 }
